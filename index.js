010235270f13d01d18e3cacf2c0349ca3608bcd4
aekeeton <ashley.keeton@outlook.com>
2022-05-25 18:30:52 -0500

    commit tree 3c2030eaa523ee8cb4640c144c88a49366beef7a

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // code your solution here
+
+function saturdayFun(activity = 'roller-skate') {
+    return `This Saturday, I want to ${activity}!`;
+}
+saturdayFun('bathe my dog');
+
+function mondayWork(work = 'go to the office') {
+    return `This Monday, I will ${work}.`;
+}
+mondayWork('work from home');
+
+function wrapAdjective(str = '*') {
+    return function(name = 'special') {
+        return `You are ${str}${name}${str}!`;
+    }
+}
